a48538e6e4023e42b4f76b5551df7ec0bfd46739
Bernhard-Reiter <account@vi4.me>
2025-08-30 14:14:19 +0200

    fix: correct import paths and tsconfig

--- packages/ui-v2/src/components/ui/button.tsx
+++ packages/ui-v2/src/components/ui/button.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
-import { cn } from "../../lib/utils";
+import { cn } from "../../utils/cn";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center font-medium transition-all focus:outline-none focus:ring-2 focus:ring-fg focus:ring-offset-2 focus:ring-offset-bg disabled:pointer-events-none disabled:opacity-50",
